11cb7ea4f676a5afe4d8a010e98e588ae164651f
VictorLi <87053334313@mail.ru>
2021-07-20 21:05:03 +0300

    Добавил 3 практическую работу

--- Практическое задание 3/Контрольные задния/Zadanie_5_sln/Zadanie_5/Zadanie_5.cpp
+++ Практическое задание 3/Контрольные задния/Zadanie_5_sln/Zadanie_5/Zadanie_5.cpp
@@ -4,39 +4,43 @@
 #include "Header.h"
 #include <vector>
 using namespace std;
+
+vector<int> v1;
 int main()
 {
     system("chcp 1251");
     std::cout << "Введи число\n";
     int a;
     cin >> a;
+    cout << "Ответ:";
     Perevod(a);
+    for (auto iterator = v1.rbegin(); iterator != v1.rend(); ++iterator)
+    {
+        std::cout << *iterator;
+    }
 }
- 
 
 
 void Perevod(int a) 
 {
-    vector<int> v1;
+ 
 
-    while (a != 0) 
+    if (a != 0) 
     {
         int b = a / 2;
         if (a==1) 
         {
             //cout << 1;
             v1.push_back(1);
-            break;
+            return;
         }
         int c = a % b;
         a = b;
         
         //cout << c;
         v1.push_back(c);
+        Perevod(a);
     }
-    cout << "Ответ:";
-    for (auto iterator = v1.rbegin(); iterator != v1.rend(); ++iterator)
-    {
-        std::cout << *iterator;
-    }
+    
+    
 }
