ddd040191989a9bfb50275d92850011182c1657b
VictorLi <87053334313@mail.ru>
2021-07-21 19:14:34 +0300

    Исправил 4практику 1 задание

--- Практическое задание 4/Контрольные задания/Exercise_1_sln/Exercise_1/Exercise_1.cpp
+++ Практическое задание 4/Контрольные задания/Exercise_1_sln/Exercise_1/Exercise_1.cpp
@@ -13,7 +13,19 @@ int main()
     double a, b, c;
     cin >> a>> b>> c;
     double x1=0, x2=0;
-    return Myroot(a, b, c, x1, x2);
+    int rez = Myroot(a, b, c, x1, x2);
+    if (rez == 1) 
+    {
+        cout << "x1 =" << x1 << "  x2 =" << x2 << endl;
+    }
+    else if (rez == 0) 
+    {
+        cout << "x = " << x1;
+    }
+    else if (rez == -1) 
+    {
+        cout << "корней нет";
+    }
 }
  
 
@@ -24,19 +36,19 @@ int Myroot(double a, double b, double c, double& x1, double& x2)
     {
         x1 = (-b + sqrt(D)) / (2 * a);
         x2 = (-b - sqrt(D)) / (2 * a);
-        cout << "x1 =" << x1 << endl << "x2 = " << x2;
+        //cout << "x1 =" << x1 << endl << "x2 = " << x2;
         return 1;
     }
     else if (D == 0) 
     {
         x1 = (-b + sqrt(D)) / (2 * a);
       
-        cout << "x =" << x1 << endl;
+       // cout << "x =" << x1 << endl;
         return 0;
     }
     else 
     {
-        cout << "корней нет";
+        //cout << "корней нет";
         return -1;
     }
 }
