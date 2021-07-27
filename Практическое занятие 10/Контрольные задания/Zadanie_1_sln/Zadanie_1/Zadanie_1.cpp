73186b43b9a69645e062d30d00422fe85dd09355
VictorLi <87053334313@mail.ru>
2021-07-27 18:03:38 +0300

    Add changers

--- Практическое занятие 10/Контрольные задания/Zadanie_1_sln/Zadanie_1/Zadanie_1.cpp
+++ Практическое занятие 10/Контрольные задания/Zadanie_1_sln/Zadanie_1/Zadanie_1.cpp
@@ -13,30 +13,19 @@ public:
     Dot* c;
     Triangle() 
     {
-        
-      
-        a= new Dot(0, 5);
-         
+   
+    }
+
+    Triangle(double a1, double b1, double c1, double e1, double f1, double g1) 
+    {
+        a = new Dot(a1, b1);
+        b = new Dot(c1, e1);
+        c = new Dot(f1, g1);
+        x1 = a->distanceTo(*b);
 
 
-        cout << "Введите координаты x и y 2-й точки";
-         
-        
-       b = new Dot(-3, -3);
-
-
-        cout << "Введите координаты x и y 3-й точки";
-      
-        
-         ;
-       c = new Dot(2, 2);
-         
-       x1 = a->distanceTo(*b);
-
-
-       x2 = c->distanceTo(*a);
-       x3 = b->distanceTo(*c);
-        
+        x2 = c->distanceTo(*a);
+        x3 = b->distanceTo(*c);
     }
 
     double x1;
@@ -82,19 +71,7 @@ public:
         x3 = b->distanceTo(*c);
     }
 
-    Triangle(double a1, double a2, double b1, double b2, double c1, double c2)
-        {
-        a = new Dot(a1, a2);
-        b = new Dot(b1, b2);
-        c = new Dot(c1, c2);
-        
-        x1 = a->distanceTo(*b);
-
-
-        x2 = c->distanceTo(*a);
-        x3 = b->distanceTo(*c);
-
-        }
+     
 
 
 
@@ -106,18 +83,21 @@ int main()
 {
     system("chcp 1251");
    //композиция потому что класс точки в данном случае сущетсвует скрыто  и создается только при создании класса треугольника
-    Triangle* tr = new Triangle();
+    double a1, b1, c1, d1, e1, f1;
+    cout << "Введите точки x1 ,x2, y1, y2 ,c1 ,c2";
+    cin >> a1 >> b1 >> c1 >> d1 >> e1 >> f1;
+    Triangle* tr = new Triangle(a1,b1,c1,d1,e1,f1);
     tr->DlinaStoron();
     tr->Perimetr();
     tr->Ploshad();
-    double x1, x2, y1, y2, c1, c2;
+   
     //агрегация потому что мы отдельно создали класс точки и он существует сам по себе, и класс точки даже не знает о существовании класса треугольника и что он в будущем будет его частью
     cout << "введи через создание Dot 6 точек x1,x2,y1,y2,z1,z2";
-    
-    cin >> x1 >> x2 >> y1 >> y2 >> c1 >> c2;
+    double x1, x2, y1, y2, z1, z2;
+    cin >> x1 >> x2 >> y1 >> y2 >> z1 >> z2;
     Dot* a = new Dot(x1, x2);
     Dot* b = new Dot(y1, y2);
-    Dot* c = new Dot(c1, c2);
+    Dot* c = new Dot(z1, z2);
     Triangle* tr2 = new Triangle(a,b,c);
     tr2->DlinaStoron();
     tr2->Perimetr();
