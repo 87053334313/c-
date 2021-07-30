be9dbe9ede40340a60b0336a5913a372c9d3be0c
VictorLi <87053334313@mail.ru>
2021-07-30 18:48:29 +0300

    Add last zadanie

---
+++ Практическое задание 15/Контрольное задание/Zadanie_1_sln/Zadanie_1/Zadanie_1.cpp
@@ -0,0 +1,60 @@
+﻿ 
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+struct StudentGrade 
+{
+public:
+    string name;
+    char ball;
+};
+
+
+
+
+int main()
+{
+    system("chcp 1251");
+    map<string, StudentGrade> MyColl;
+    StudentGrade viktor;
+    viktor.name = "Viktor";
+    viktor.ball = '5';
+    StudentGrade sasha;
+    sasha.name = "Sasha";
+    sasha.ball = 'B';
+    StudentGrade misha;
+    misha.name = "Misha";
+    misha.ball = '3';
+
+    MyColl["Viktor"] = viktor;
+    MyColl["Sasha"] = sasha;
+    MyColl["Misha"] = misha;
+    string otvet;
+    do {
+        cout << "\n";
+        cout << "У вас есть три ученика , вы моежет по имени изменить значение оценки";   cout << "\n";
+        for (map<string, StudentGrade>::iterator it = MyColl.begin(); it != MyColl.end(); ++it)
+        {
+            cout << "Name = " << it->first << "\t" << "Otsenka =" << it->second.ball << "  \n";
+        }
+        cout << "Введите имя ученика, у которого хотите изменить оценку\n";
+        string name;
+        cin >> name;
+        cout << "Введите его оценку\n";
+        char o;
+        cin >> o;
+
+        MyColl[name].ball = o;
+        for (map<string, StudentGrade>::iterator it = MyColl.begin(); it != MyColl.end(); ++it)
+        {
+            cout << "Name = " << it->first << "\t" << "Otsenka =" << it->second.ball << "  \n";
+        }
+       
+        cout << "Выйти?(y/n)";
+        cin >> otvet;
+            
+    
+    } while (otvet != "y");
+} 
